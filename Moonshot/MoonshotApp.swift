7c183e00a7ac05ba5c7170cb04a8bff37c394740
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-19 13:41:18 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Gabriel Marquez on 2022-06-19.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
