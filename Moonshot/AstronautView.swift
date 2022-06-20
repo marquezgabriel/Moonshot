077dcafd0ea676a9dfa275c66b7d1e0269446c48
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-19 21:13:05 -0400

    Complete day 41

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,37 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Gabriel Marquez on 2022-06-19.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
+
