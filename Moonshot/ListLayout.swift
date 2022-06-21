7ae147df45626cdc9c8c73863c56cf9d194ed013
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-21 16:39:56 -0400

    Almost complete day 42

---
+++ Moonshot/ListLayout.swift
@@ -0,0 +1,48 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by Gabriel Marquez on 2022-06-21.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        List(missions) {mission in
+            NavigationLink {
+                MissionView(mission: mission, astronauts: astronauts)
+            } label: {
+                HStack{
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                        .padding()
+                    
+                    VStack (alignment: .leading){
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                        
+                    }
+                }
+            }
+            .listRowBackground(Color.darkBackground)
+        }
+        
+        .listStyle(.plain)
+    }
+}
+
+struct ListLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        ListLayout(astronauts: Bundle.main.decode("astronaunts.json"), missions: Bundle.main.decode("missions.json"))
+            .preferredColorScheme(.dark)
+        
+    }
+}
